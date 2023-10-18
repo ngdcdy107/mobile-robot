2d176c7c26e30503d4c180169038a997559afe57
duy <duyducnguyen107@gmail.com>
2023-10-18 17:11:46 +0700

    thanks

--- thanks.py
+++ thanks.py
@@ -1,1 +1,17 @@
-import speech_recognition as sr
+import openai
+from noi import noi
+from nghe0 import nghe0
+noi("Mời bạn nói ")
+while True:
+    text = nghe0()
+    if "Hello" in text or "chào" in text:
+        noi("chào bạn")
+    elif "tên" in text:
+        noi("tôi là trợ lý ảo quán trà ai")
+    elif "tạm biệt" in text:
+        noi("bye")
+        break
+    elif "..." in text:
+        noi("Xin lỗi,tôi không nghe được rõ")
+    else:
+        noi("xin lỗi tôi chưa thông minh nên không hiểu câu này")
